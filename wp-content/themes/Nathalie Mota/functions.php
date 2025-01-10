aa478de3ac5d9253bcefc24f930ae54f4baf62be
Maxence LE CLERC <shadange@hotmail.com>
2025-01-10 11:10:09 +0100

    CPT, SCF, Ajout des photos

--- wp-content/themes/Nathalie Mota/functions.php
+++ wp-content/themes/Nathalie Mota/functions.php
@@ -6,11 +6,13 @@ add_theme_support('post-thumbnails' );
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
 
+//Ajout du menu
 function declarer_emplacement_menu() {
   register_nav_menu('principal', __('Menu principal'));
 }
 add_action('after_setup_theme', 'declarer_emplacement_menu');
 
+//Chargement du fichier CSS
 function enqueue_custom_css() {
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/style.css' );
 }
@@ -20,6 +22,7 @@ function custom_theme_setup() {
 }
 add_action( 'after_setup_theme', 'custom_theme_setup' );
 
+// Chargement du fichier JS
 function enqueue_scripts() {
     wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/scripts/script.js', array(), '1.0', true );
 }
